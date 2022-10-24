26a233051d15db3ab7200cfa863585b8926306e9
TheOpti <wieckowski.piotr.93@gmail.com>
2022-10-24 12:10:04 +0200

    Counter component

--- src/routes/counter/index.tsx
+++ src/routes/counter/index.tsx
@@ -1,5 +1,31 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore, useClientEffect$ } from '@builder.io/qwik';
 
 export default component$(( ) => {
-  return <span>Hello world</span>
+  return (
+    <>
+      <span>Hello world</span>
+      <div>
+        <Clock />
+      </div>
+    </>
+  )
+});
+
+export const Clock = component$(() => {
+  const state = useStore({ time: '' });
+
+  useClientEffect$(() => {
+    console.debug('inside useClientEffect');
+    const update = () => {
+      console.debug('inside update');
+      state.time = new Date().toLocaleTimeString();
+    }
+
+    update();
+
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  });
+
+  return <span>{state.time}</span>
 });
